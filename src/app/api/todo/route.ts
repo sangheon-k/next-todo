c527f7481a163dd576ad976741328bcfc318e4cd
sangheon-k <sangheon.k@kakao.com>
2024-07-15 23:30:03 +0900

    feat : getTodos - Next Api

---
+++ src/app/api/todo/route.ts
@@ -0,0 +1,14 @@
+import { createServerSideClient } from "@/lib/supabase";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  const supabase = await createServerSideClient();
+
+  const result = await supabase
+    .from("todos_no_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .order("id", { ascending: false });
+
+  return NextResponse.json({ ...result });
+};
