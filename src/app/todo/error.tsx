289eec7ed7024e1ec8927647bff0d7f686b5fa62
sangheon-k <sangheon.k@kakao.com>
2024-07-31 01:29:03 +0900

    Feat : Todo 페이지 추가 (SSR)

---
+++ src/app/todo/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <h2 className=" text-9xl mt-10">404</h2>
+      <p className="text-2xl mt-5">Not Found</p>
+    </div>
+  );
+};
+
+export default Error;
