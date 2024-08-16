a6ea0abfc2a34056e2b8d0d6eb0a111abb544f76
sangheon-k <sangheon.k@kakao.com>
2024-08-16 11:37:18 +0900

    Feat : ServerActions의 다양한 활용

--- src/app/todo/components/ClientComopnentTest.tsx
+++ src/app/todo/components/ClientComopnentTest.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { pingAction } from "@/actions/ping/ping.action";
+import { getTodoAction } from "@/actions/todo/\btodo.action";
 import React from "react";
 
 const ClientComopnentTest = () => {
   const handleClick = async () => {
-    const result = await pingAction();
-    console.log(result);
+    const result = await getTodoAction();
+    console.log("handleClick result", result);
   };
   return (
     <div>
--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -2,18 +2,26 @@ import { pingAction } from "@/actions/ping/ping.action";
 import { sleep } from "@/lib/utils";
 import React from "react";
 import ClientComopnentTest from "./components/ClientComopnentTest";
+import { getTodoAction } from "@/actions/todo/\btodo.action";
+
+/**
+ * 1. ServerAction + Route
+ * 2. ServerAction + SSR
+ * 3. ServerAction + CSR
+ */
 
 const page = async () => {
   console.log(">> SSR Start");
 
   await sleep(1500);
 
-  const res = await pingAction();
+  const res = await getTodoAction();
 
   console.log(">> SSR End");
 
   return (
     <div>
+      todo page {JSON.stringify(res)}
       <ClientComopnentTest />
     </div>
   );
